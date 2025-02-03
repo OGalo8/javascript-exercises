91f8cccd80900a0fb0c36016d58f2cf467938eda
ogalo <arielgalo39481@gmail.com>
2025-02-03 15:37:47 -0600

    Solving exercise 1

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+  let reversedWord = "";
+  for (let letterIndex = word.length - 1; letterIndex >= 0; letterIndex--) {
+    reversedWord += word.at(letterIndex);
+  }
+  return reversedWord;
 };
 
 // Do not edit below this line
