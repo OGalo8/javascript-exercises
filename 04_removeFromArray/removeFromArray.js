d3382c4a099142d278e52aafcc65007fd955dd82
ogalo <arielgalo39481@gmail.com>
2025-02-03 21:51:05 -0600

    Solving exercises 2,3 and 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function (
+  originalArray,
+  firstRemovedElement,
+  ...otherRemovedElements
+) {
+  let elementsToErase = [firstRemovedElement, otherRemovedElements].flat();
+  let cleanArray = [];
+  for (let element of originalArray) {
+    if (!elementsToErase.includes(element)) {
+      cleanArray.push(element);
+    }
+  }
+
+  return cleanArray;
 };
 
 // Do not edit below this line
