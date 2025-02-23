24fa2f1228972e279265df2125dbeec9b1505900
ogalo8 <arielgalo39481@gmail.com>
2025-02-23 13:52:14 -0600

    Solving exercises 8 and 9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((accumulator, current) => accumulator + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((accumulator, current) => accumulator * current, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a == 0) return 1;
+  let factors = [];
+  for (let i = a; i > 0; i--) {
+    factors.push(i);
+  }
+  return multiply(factors);
 };
 
 // Do not edit below this line
@@ -29,5 +34,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,32 @@
-const palindromes = function () {
+// # Exercise 09 - Palindromes
 
+// Write a function that determines whether or not a given string is a palindrome.
+
+// A palindrome is a string that is spelled the same both forwards and backwards, usually without considering punctuation or word breaks:
+
+// ### some palindromes:
+//   - A car, a man, a maraca.
+//   - Rats live on no evil star.
+//   - Lid off a daffodil.
+//   - Animal loots foliated detail of stool lamina.
+//   - A nut for a jar of tuna.
+
+// ```javascript
+// palindromes('racecar') // true
+// palindromes('tacos') // false
+// ```
+
+const palindromes = function (sentence) {
+  let myRegex = /[a-zA-Z0-9]+/g;
+  let straightLetters = sentence
+    .toLowerCase()
+    .match(myRegex)
+    .join("")
+    .split("");
+  let reversedLetters = [...straightLetters].reverse(); //we clone the array straightLetters
+  return straightLetters.every(
+    (value, index) => straightLetters[index] == reversedLetters[index]
+  );
 };
 
 // Do not edit below this line
