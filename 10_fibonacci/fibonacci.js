24e592e61d68a4228be9c5e8d0fa3ca1d6f2b710
ogalo8 <arielgalo39481@gmail.com>
2025-02-23 15:18:38 -0600

    Solving exercises 10,11,12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+// # Exercise 10 - Fibonacci
 
+// Create a function that returns a specific member of the Fibonacci sequence:
+
+// > A series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers.
+// > In this exercise, the Fibonacci sequence used is 1, 1, 2, 3, 5, 8, etc.
+// > To learn more about Fibonacci sequences, go to: https://en.wikipedia.org/wiki/Fibonacci_sequence
+
+// ```javascript
+// fibonacci(4); // returns the 4th member of the series: 3  (1, 1, 2, 3)
+// fibonacci(6); // returns 8
+// ```
+
+const fibonacci = function (n) {
+  let num = Number(n);
+  if (num < 0) return "OOPS";
+  if (num == 0) return 0;
+  if (num == 1) return 1;
+
+  return fibonacci(num - 1) + fibonacci(num - 2);
 };
 
-// Do not edit below this line
+// // Do not edit below this line
 module.exports = fibonacci;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  let currentYear = new Date().getFullYear();
+  let sortedPersons = array.sort((first, second) => {
+    let firstAge = (first.yearOfDeath || currentYear) - first.yearOfBirth;
+    let secondAge = (second.yearOfDeath || currentYear) - second.yearOfBirth;
+    return secondAge - firstAge;
+  });
+  return sortedPersons[0];
 };
 
 // Do not edit below this line
